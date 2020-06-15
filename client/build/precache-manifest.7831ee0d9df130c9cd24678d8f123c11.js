dc7da907130246b98ecdc75b3d3092f0e3612063
ManaN Patel <mananpatel320@gmail.com>
2020-06-15 17:16:07 +0530

    first commit

---
+++ client/build/precache-manifest.7831ee0d9df130c9cd24678d8f123c11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecab9683946d01c128b9f1ec1f983338",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9795251c9cfb84e5dc",
+    "url": "/static/css/main.28ec31e2.chunk.css"
+  },
+  {
+    "revision": "c197f6111def9dc08c70",
+    "url": "/static/js/2.ad571d88.chunk.js"
+  },
+  {
+    "revision": "d4773cfc8952e56a2df4dd3d885d1f23",
+    "url": "/static/js/2.ad571d88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9795251c9cfb84e5dc",
+    "url": "/static/js/main.21d2b59f.chunk.js"
+  },
+  {
+    "revision": "6f0234b4b87d585d06e8",
+    "url": "/static/js/runtime-main.cfae3202.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  }
+]);
---
+++ config/production.js
@@ -0,0 +1,6 @@
+{
+    "mongoURI": "mongodb+srv://mananpatel320:mananpatel320@sonic-mlkoh.mongodb.net/<dbname>?retryWrites=true&w=majority",
+    "jwtSecret": "secret",
+    "githubToken": "<yoursecrectaccesstoken>"
+  }
+  
